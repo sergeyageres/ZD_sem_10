8c7eaa850a5451cf1e9d0faef8a9cafaa530bc8c
Sergey <serega110193@mail.ru>
2022-11-19 18:52:47 +0900

    добавдение задач

---
+++ zadacha_64/Program.cs
@@ -0,0 +1,20 @@
+﻿ //  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+ 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
